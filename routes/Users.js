51ccb2096f8c026e56978a710495d11f33460ec6
jencely <jeny.guanuna@gmail.com>
2020-09-10 10:33:33 -0500

    Proyecto integrador Ingequimed V0.1

---
+++ routes/Users.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/User')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+//registro
+users.post('/register', (req, res) => {
+  const today = new Date()
+  const userData = {
+    nombre: req.body.nombre,
+    email: req.body.email,
+    password: req.body.password,
+    created: today
+  }
+
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(user =>{
+     if(!user){
+        const hash = bcrypt.hashSync(userData.password,10)
+        userData.password = hash
+        User.create(userData)
+        .then(user => {
+           let token = jwt(user.dataValues, process.env.SECRET_KEY,{
+              expiresIn:1440
+           })
+           res.json({token: token})
+        })
+        .catch(err => {
+           res.send('error:'+ error)
+        })
+     } else {
+        res.json({error: 'El usuario ya existe'})
+     }
+  })
+  .catch(err => {
+     res.send('error: ' + error)
+  })
+})
+//login
+
+users.post('/login', (req, res) => {
+   User.findOne({
+     where: {
+       email: req.body.email
+     }
+   })
+     .then(user => {
+       if (user) {
+         if (bcrypt.compareSync(req.body.password, user.password)) {
+           let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+             expiresIn: 1440
+           })
+           res.json({token: token})
+         }
+       } else {
+         res.send( 'El usuario no existe' )
+       }
+     })
+     .catch(err => {
+       res.send('error: ' + err)
+     })
+ })
+
+ //perfil
+ users.get('/profile', (req, res) => {
+   var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+ 
+   User.findOne({
+     where: {
+       id: decoded.id
+     }
+   })
+     .then(user => {
+       if (user) {
+         res.json(user)
+       } else {
+         res.send('El usuario no existe')
+       }
+     })
+     .catch(err => {
+       res.send('error: ' + err)
+     })
+ })
+ 
+module.exports = users;
+
+   
